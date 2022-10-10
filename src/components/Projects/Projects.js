46269b58155ad77bb7cea3d1f55cda3d3f486afc
Cian Butler <cianbtlr3@gmail.com>
2022-10-10 21:21:54 +0100

    Project Fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,34 +9,10 @@ const Projects = () => (
     <SectionDivider/>
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map((p, i) => {
-        return (
-          <BlogCard key={i}>
+          <BlogCard>
           <Img src={`${process.env.NEXT_PUBLIC_IMAGES}/images/1.png`} />
             <TitleContent>
-              <HeaderThree title>{p.title}</HeaderThree>
-              <Hr />
-            </TitleContent>
-            <CardInfo className="card-info">{p.description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {p.tags.map((t, i) => {
-                  return <Tag key={i}>{t}</Tag>;
-                })}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks href={p.visit} target="_blank">Code</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        );
-      })}
-
-          <BlogCard key={i}>
-          <Img src={`${process.env.NEXT_PUBLIC_IMAGES}/images/1.png`} />
-            <TitleContent>
-              <HeaderThree title></HeaderThree>
+              <HeaderThree title>Portfolio</HeaderThree>
               <Hr />
             </TitleContent>
             <CardInfo className="card-info">Using React, Node.js & MongoDB I created a portfolio. It is a clean, functional, and responsive web application. The aim of this project was to provide a website to display information about myself while also teaching myself more about React and JavaScript.</CardInfo>
@@ -51,6 +27,47 @@ const Projects = () => (
             </div>
             <UtilityList>
               <ExternalLinks href="https://github.com/CBtlr00/Portfolio" target="_blank">Code</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+
+          <BlogCard>
+          <Img src={`${process.env.NEXT_PUBLIC_IMAGES}/images/2.png`} />
+            <TitleContent>
+              <HeaderThree title>E-Commerce</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">Using Django and Python I built a E-Commerce Store. I learnt how to build a Full Stack Django Application - from start to finish. It is a simple e-commerce store app that allows users to buy item and create accounts to shop online.</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                <Tag>Django</Tag>
+                <Tag>Python</Tag>
+                <Tag>HTML</Tag>
+                <Tag>TailWind</Tag>
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href="https://github.com/CBtlr00/eCommerce-Store" target="_blank">Code</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+
+          <BlogCard>
+          <Img src={`${process.env.NEXT_PUBLIC_IMAGES}/images/3.png`} />
+            <TitleContent>
+              <HeaderThree title>Movie API</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo className="card-info">Using vanilla Javascript and OMDB API I created a Movie APi. Whilst doing this I learnt more on server-side and client-side in web application.</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                <Tag>JavaScript</Tag>
+                <Tag>HTML</Tag>
+                <Tag>CSS</Tag>
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href="https://github.com/CBtlr00/MovieAPI" target="_blank">Code</ExternalLinks>
             </UtilityList>
           </BlogCard>
     </GridContainer>
